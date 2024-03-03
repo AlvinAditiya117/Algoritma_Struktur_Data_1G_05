a6c2ff9c197a341794b344efff91a32f77df6d27
AlvinAditiya117 <ryuzavin@gmail.com>
2024-03-03 21:10:28 +0700

    P3_percobaan3_05

---
+++ P3/BalokMenu.java
@@ -0,0 +1,13 @@
+public class BalokMenu {
+    public static void main(String[]args){
+        Balok[]bilArray = new Balok[3];
+    bilArray[0] = new Balok(100,30,12);
+    bilArray[1] = new Balok(120,40,15);
+    bilArray[2] = new Balok(210,50,25);
+    
+    for (int i = 0; i < 3; i++)
+    {
+        System.out.println("volume balok ke"+i+"="+bilArray[i].hitungVolume());
+    }
+}
+}
