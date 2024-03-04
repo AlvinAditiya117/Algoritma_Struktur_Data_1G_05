6ea26e7a9ec76ed24514dfbe14694422f46dab1b
AlvinAditiya117 <ryuzavin@gmail.com>
2024-03-05 02:34:07 +0700

    P3_percobaan2_pertanyaan4_05

--- P3/PersegiPanjang.java
+++ P3/PersegiPanjang.java
@@ -4,11 +4,14 @@ public class PersegiPanjang {
     public int panjang;
     public int lebar;
 public static void main(String[]args) {
-    PersegiPanjang[] ppArray = new PersegiPanjang[3];
+
+    System.out.println("masukkan jumlah persegi panjang : ");
 
     Scanner sc05 = new Scanner(System.in);
 
-    for (int i = 0; i < 3; i++) {
+    PersegiPanjang[] ppArray = new PersegiPanjang[9];
+
+    for (int i = 0; i < ppArray.length; i++) {
         
 ppArray[i] = new PersegiPanjang();
 System.out.println("persegi panjang ke-"+i);
@@ -19,7 +22,7 @@ ppArray[i].lebar = sc05.nextInt();
 
  }
 
- for (int i = 0; i < 3; i++) {
+ for (int i = 0; i < ppArray.length; i++) {
     System.out.println(" persegi panjang ke-"+i);
     System.out.print("panjang : "+ppArray[i].panjang+", lebar : "+ppArray[i].lebar);
     }
