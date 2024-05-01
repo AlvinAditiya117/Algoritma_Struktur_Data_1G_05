e727dddc8fb2eb465159bacdddef585c40b8f61b
AlvinAditiya117 <ryuzavin@gmail.com>
2024-05-01 17:59:18 +0700

    P9_percobaan1_05

---
+++ P9/Gudang05.java
@@ -0,0 +1,67 @@
+public class Gudang05 {
+    Barang05[] tumpukan;
+    int size;
+    int top;
+
+    public Gudang05(int kapasitas) {
+        size = kapasitas;
+        tumpukan = new Barang05[size];
+        top  = -1;
+    }
+    public boolean cekKosong(){
+        if (top == -1) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean cekPenuh(){
+        if (top == size -1) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void tambahBarang(Barang05 brg){
+        if (!cekPenuh()) {
+            top++;
+            tumpukan[top] = brg;
+            System.out.println("Barang " + brg.nama + " berhasil ditambah kegudang");
+        }else{
+            System.out.println("gagal!! tumpukan barang digudang sudah penuh");
+        }
+    }
+    public Barang05 ambilBarang(){
+        if(!cekKosong()){
+            Barang05 delete = tumpukan[top];
+            top--;
+            System.out.println("barang "+delete.nama+" diambil dari gudang");
+            return delete;
+        }else{
+            System.out.println("tumpukan barang kosong");
+            return null;
+        }
+    }
+    public Barang05 lihatBarangTeratas(){
+        if (top== size-1) {
+            Barang05 barangTeratas = tumpukan[top];
+            top--;
+            System.out.println("barang "+barangTeratas.nama+" diambil dari gudang");
+            return barangTeratas;
+        }else{
+            System.out.println("tumpukan barang kosong");
+            return null;
+        }
+    }
+    public void tampilkanBarang(){
+        if (!cekKosong()) {
+            System.out.println("rincian tupukkan barang digudang");
+            for (int i = 0; i <= top; i++){
+                System.out.printf("kode %d: %s (kategori %s)\n", tumpukan[i].kode, tumpukan[i].nama,tumpukan[i].kategori);
+            }
+        }else{
+            System.out.println("tumpukan barang kosong");
+        }
+    }
+
+}
---
+++ P9/Main05.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main05 {
+    public static void main(String[] args) {
+        Gudang05 gudang = new Gudang05(7);
+        Scanner sc05 = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("<===( menu )===>");
+            System.out.println("1.tambah barang");
+            System.out.println("2.ambil barang");
+            System.out.println("3.tampilkan tumpukan");
+            System.out.println("4.keluar");
+            System.out.print("pilih operasi >--> ");
+            int pilihan = sc05.nextInt();
+            sc05.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("masukkan kode barang : ");
+                    int kode = sc05.nextInt();
+                    System.out.print("masukkan nama barang : ");
+                    String nama = sc05.next();
+                    System.out.print("masukkan nama kategori : ");
+                    String kategori = sc05.next();
+                    Barang05 barangBaru = new Barang05(kode, nama, kategori);
+                    gudang.tambahBarang(barangBaru);
+                    break;
+                case 2:
+                    gudang.ambilBarang();
+                    break;
+                case 3:
+                    gudang.tampilkanBarang();
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("pilihan tidak valid. silakan coba lagi");
+            }
+        }
+    }
+}
