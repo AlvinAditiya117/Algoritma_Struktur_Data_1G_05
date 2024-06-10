5fa2fd93ec1018f145c24101762aa843a8bb1f2f
AlvinAditiya117 <ryuzavin@gmail.com>
2024-06-10 21:31:43 +0700

    P14_perbaikan Grphmain_05

--- P15/GraphMain05.java
+++ P15/GraphMain05.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class GraphMain05 {
     public static void main(String[] args) throws Exception {
@@ -13,5 +14,42 @@ public class GraphMain05 {
         gedung.removeEdge(1, 3);
         gedung.printGraph();
 
+        Scanner sc05 = new Scanner(System.in);
+        boolean lanjut = true;
+        while (lanjut) {
+            System.out.println("Menu:");
+            System.out.println("1. Cek gedung");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu (0/1): ");
+            int pilihan = sc05.nextInt();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan asal gedung (0-5): ");
+                    int asal = sc05.nextInt();
+                    System.out.print("Masukkan gedung tujuan (0-5): ");
+                    int tujuan = sc05.nextInt();
+
+                    if (asal < 0 || asal > 5 || tujuan < 0 || tujuan > 5) {
+                        System.out.println("Gedung yang dimasukkan tidak valid. Masukkan gedung dalam rentang 0-5.");
+                    } else {
+                        if (gedung.adjacency(asal, tujuan)) {
+                            System.out.println(
+                                    "Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " bertetangga");
+                        } else {
+                            System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan)
+                                    + " tidak bertetangga");
+                        }
+                    }
+                    break;
+                case 0:
+                    lanjut = false;
+                    System.out.println("Terima kasih telah menggunakan program.");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan pilih lagi.");
+                    break;
+            }
+        }
     }
 }
